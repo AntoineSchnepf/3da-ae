2beb501d33bd277d1b3527a127797492f9196cc8
Antoine Schnepf <antoinesnpf@gmail.com>
2025-02-27 13:41:18 +0000

    License

--- ae/trainers.py
+++ ae/trainers.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Antoine Schnepf, Karim Kassab
+# Copyright 2023 Antoine Schnepf, Karim Kassab
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
